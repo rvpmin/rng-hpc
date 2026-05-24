5bd5539c479e4b2c826c51ba86d0310dbd24c2f5
Roxana Perez <roxanaperezmedina6@gmail.com>
2026-05-24 05:05:44 -0600

    Updated bash script

--- barber/benchmark.sh
+++ barber/benchmark.sh
@@ -10,13 +10,14 @@ for np in "${PROCS[@]}"; do
     echo "Ejecutando con $np procesos..."
 
     #result=$(mpiexec -n $np -f /data/user1/mymachinefile ./rng $N)
-    result=$(mpiexec -n $np -f /data/user1/mymachinefile -env FI_PROVIDER tcp ./rng_mpi $N)
+    result=$(mpiexec -n $np -f /data/user1/mymachinefile /data/user1/rng-hpc/barber/rng_mpi $N)
     
     real=$(echo "$result" | grep "^real" | awk '{print $2}')
     user=$(echo "$result" | grep "^user" | awk '{print $2}')
     sys=$(echo "$result" | grep "^sys"  | awk '{print $2}')
     
     echo "$np $real $user $sys" >> $OUTPUT
+    sleep 3
 done
 
 echo "Tiempos guardados en $OUTPUT"
